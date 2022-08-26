82363bcaebce61312701d490add9ab86e254528e
holgerbrandl@gmail.com <holgerbrandl@users.noreply.github.com>
2022-08-26 14:28:39 +0200

    v4.1.0 release

--- kscript.rb
+++ kscript.rb
@@ -1,8 +1,8 @@
 class Kscript < Formula
   desc "kscript"
   homepage "https://github.com/holgerbrandl/kscript"
-  url "https://github.com/holgerbrandl/kscript/releases/download/v4.0.3/kscript-4.0.3-bin.zip"
-  sha256 "e92b000ea13dd48c49329269b025d59c3e0ff507fe3a357fb5af401773b1cdbb"
+  url "https://github.com/holgerbrandl/kscript/releases/download/v4.1.0/kscript-4.1.0-bin.zip"
+  sha256 "02c3a1c414d9fab6fd05cb41e3f5d50e0e590226edf6b44d80f779e71d8e5942"
 
   depends_on "kotlin"
 
